c0fae47999b6b01b602e5c8318dbddec915b4061
ahmedzatar <ahmed.zatar2@gmail.com>
2021-07-27 13:21:54 +0300

    first commit

---
+++ stories/views.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.shortcuts import render
+from rest_framework import generics
+
+from .models import Story
+from .serializer import StoriesSerializer
+
+# Create your views here.
+class StoriesListView(generics.ListCreateAPIView):    
+    serializer_class = StoriesSerializer
+    queryset = Story.objects.all()
+
+class StoryDetailsView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = StoriesSerializer
+    queryset = Story.objects.all()
